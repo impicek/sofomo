4cab1e294c672008f2b20a0e9a3b9698fd6f2c81
impicek <piotr.omelczuk@dataforce.pl>
2022-03-16 17:20:01 +0100

    a

--- po.sql
+++ po.sql
@@ -1,8 +1,9 @@
-select c.dimension_1, map.correct_dimension_2, c.measure_1, c.measure_2 from (
-select distinct a.DIMENSION_1, a.DIMENSION_2, a.measure_1, 0 measure_2
+select dimension_1, correct_dimension_2 dimension_2, sum(measure_1) measure_1, sum(measure_2)measure_2 from (
+select distinct c.dimension_1, map.correct_dimension_2, c.measure_1, c.measure_2 from (
+select  a.DIMENSION_1, a.DIMENSION_2, a.measure_1, 0 measure_2
 from a
 union all
-select distinct b.DIMENSION_1, b.DIMENSION_2, 0, b.measure_2
+select b.DIMENSION_1, b.DIMENSION_2, 0, b.measure_2
 from b) c, map
-where c.dimension_1 = map.dimension_1
-group by c.dimension_1, map.correct_dimension_2, c.measure_1, c.measure_2
+where c.dimension_1 = map.dimension_1)
+group by dimension_1, correct_dimension_2
